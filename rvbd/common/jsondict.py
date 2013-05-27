bdef3444242d68ce8748b1153cc0316c63048708
Christopher J. White <cwhite@riverbed.com>
2013-05-26 21:29:52 -0400

    Added new JsonDict class in rvbd.common.jsondict

---
+++ rvbd/common/jsondict.py
@@ -0,0 +1,205 @@
+# Copyright (c) 2013 Riverbed Technology, Inc.
+#
+# This software is licensed under the terms and conditions of the
+# MIT License set forth at:
+#   https://github.com/riverbed/flyscript-portal/blob/master/LICENSE ("License").
+# This software is distributed "AS IS" as set forth in the License.
+
+import json
+
+class JsonDict(dict):
+    _default = None
+    _required = None
+
+    """
+    Creates a dictionary, setting attributes for each toplevel key in
+    the dictionary.  This allows simpler access to the top-level keys
+    using dotted notation rather than the square brackets access
+
+    The object is dict and can be maniuplated and iterated just like a
+    dict, including nested dictionaries.  
+
+    Example:
+      >>> d = JsonDict({'foo': 1,
+                        'bar': 'This is the bar',
+                        'baz': { 'fozzle': 10,
+                                 'frobble': 'Flounder'}})
+      >>> d.foo
+      1
+      >>> d['foo']
+      1
+      >>> d
+      {'foo': 1,
+       'bar': 'This is the bar',
+       'baz': { 'fozzle': 10,
+                'frobble': 'Flounder'}}
+
+      >>> type(d.baz)
+      rvbd.common.jsondict.JsonDict
+      >>> d.baz.fozzle
+      10
+    """
+    # We do not need to override the 'hasattr' as internally this
+    # method invokes 'getattr' method and if 'getattr' returns
+    # AttributeError then 'hasattr' returns False else it returns True.
+
+    def __init__(self, dict=None, default=None, **kwargs):
+        """Create a new JsonDict object.
+
+        `dict` is an optional dictionary to initialize from
+
+        `default` is an optional structure used to define defaults
+            as well as validate future assignments
+
+        Additional keyword arguments are parsed as if passed
+        to __setattr__.  
+        """
+        super(JsonDict, self).__init__()
+        if default:
+            self._default = default
+        self.update(self._default)
+        self.update(dict)
+        self.update(kwargs)
+
+        if self._required is not None:
+            for key in self._required:
+                if self.__getattr__(key) is None:
+                    raise KeyError("Required key not specified: %s" % '.'.join(key.split("__")))
+
+    def __dir__(self):
+        return self.keys()
+        
+    def __str__(self):
+        """Return the json-encoded form of the dictionary."""
+        return json.dumps(self)
+
+    @classmethod
+    def loads(cls, s):
+        """Create a JsonDict object from a json-encoded string."""
+        o = cls()
+        o.parse(s)
+        return o
+    
+    def parse(self, s):
+        """Update the object from a json-encoded string."""
+        d = json.loads(s)
+        self.update(d)
+        
+    def update(self, dict):
+        """Update the object from a dict."""
+        if dict is not None:
+            for k,v in dict.iteritems():
+                self.__setattr__(k, v)
+    
+    def __getattr__(self, key):
+        """
+        Return the value associated with the specified key.  Keys starting
+        with an underscore are assumed to be standard attributes.  All other
+        keys are treated as members of the dictionary.
+
+        The key may also reference nested dictionaries using '__' to split
+        key names at each level.
+
+        The following representations are equivalent:
+
+        >>> d = JsonDict({'name' : { 'first' : 'John' }})
+        >>> d['name']['first']
+        'John'
+        >>> d.name.first
+        'John'
+        >>> d.name__first
+        'John'
+        
+        This last form using '__' is useful as part of initialization.
+        """
+
+        # Treat keys leading with '_' as standard attributes
+        if key[0] == '_':
+            return object.__getattr__(self, key)
+
+        # All other keys are assumed to be part of the dictionary
+        
+        # Treat "__" as a separator like a '.'
+        #   so x.a__b ==> x.a.b
+        keyparts = key.split("__")
+
+        # Navigate to the right nested object
+        obj = self
+        for k in keyparts:
+            #print "looking at %s" % k
+            if isinstance(obj, list):
+                obj = obj[int(k)]
+            elif isinstance(obj, dict):
+                if (k not in obj):
+                    raise AttributeError(key)
+                obj = obj[k]
+            else:
+                raise AttributeError(key)
+                
+        return obj
+    
+    def __setattr__(self, key, value):
+        """
+        Set the value associated with the specified key.  Keys starting
+        with an underscore are assigned as standard attributes.  All others
+        are assigned to the dictionary object.  
+
+        The key may also reference nested dictionaries using '__' to split
+        key names at each level.
+
+        See __getattr__ as an example.
+        """
+        
+        #print "setattr: self <<%s>> : setattr(%s => %s)" % (str(self), key, value)
+
+        # Turn "obj.<key> = <value>" into "obj['<key>'] = <value>
+        if key[0] == '_':
+            # Treat keys starting with an underscore as normal attributes
+            return object.__setattr__(self, key, value)
+
+        # Treat "__" as a separator like a '.'
+        #   so x.a__b ==> x.a.b
+        if isinstance(key, unicode):
+            key = key.encode('utf-8')
+        keyparts = key.split("__")
+
+        # Navigate to the parent object to be set. 
+        obj = self
+        for key in keyparts[:-1]:
+            if ((not isinstance(obj, dict)) or
+                ((obj._default is not None) and (key not in obj._default))):
+                raise KeyError("Invalid key: '%s'" % key)
+
+            if (key not in obj) or (not isinstance(obj[key], dict)):
+                obj[key] = JsonDict()
+                if obj._default is not None:
+                    obj[key]._default = obj._default[key]
+
+            obj = obj[key]
+
+        key = keyparts[-1]
+
+        if ((not isinstance(obj, dict)) or
+            ((obj._default is not None) and (key not in obj._default))):
+            raise KeyError("Invalid key: '%s'" % key)
+
+        obj[key] = self._decode(value, obj._default[key] if obj._default is not None else None)
+
+    def _decode(self, value, default):
+        """Internal function to decode a value, replacing standard dicts with
+        JsonDict."""
+        #print "decode(%s, %s)" % (value, default)
+        if isinstance(value, dict):
+            newvalue = JsonDict(dict=value, default=default)
+        elif isinstance(value, list):
+            newvalue = []
+            for item in value:
+                newvalue.append(self._decode(item, None))
+        else:
+            if isinstance(value, unicode):
+                newvalue = value.encode('utf-8')
+            else:
+                newvalue = value
+        
+        return newvalue
+
---
+++ rvbd/common/test/test_jsondict.py
@@ -0,0 +1,147 @@
+# Copyright (c) 2013 Riverbed Technology, Inc.
+#
+# This software is licensed under the terms and conditions of the 
+# MIT License set forth at:
+#   https://github.com/riverbed/flyscript/blob/master/LICENSE ("License").  
+# This software is distributed "AS IS" as set forth in the License.
+
+from rvbd.common.jsondict import JsonDict
+
+import unittest
+import logging
+import datetime
+
+try:
+    from testconfig import config
+except ImportError:
+    if __name__ != '__main__':
+        raise
+    config = {}
+
+logger = logging.getLogger(__name__)
+logging.basicConfig(level=logging.DEBUG,
+                    format="%(asctime)s [%(levelname)-5.5s] %(msg)s")
+
+class JsonDictTest(unittest.TestCase):
+
+    def test_init_dict1(self):
+        j = JsonDict(dict={'first' : 'John',
+                           'last' : 'Doe',
+                           'age' : 1})
+        self.assertEqual(j.first, 'John')
+        self.assertEqual(j.last, 'Doe')
+        self.assertEqual(j.age, 1)
+        self.assertEqual(j.first, j['first'])
+
+        j.first = 'Judy'
+        self.assertEqual(j.first, 'Judy')
+        self.assertEqual(j.last, 'Doe')
+
+    def test_init_dict2(self):
+        j = JsonDict(dict={'name' : {'first' : 'John',
+                                     'last' : 'Doe'},
+                           'age' : 1})
+        self.assertEqual(j.name.first, 'John')
+        self.assertEqual(j.name.last, 'Doe')
+        self.assertEqual(j.age, 1)
+        self.assertEqual(j.name, {'first' : 'John', 'last' : 'Doe'})
+
+        j.name.first = 'Joe'
+        j.name.last = 'Blow'
+        self.assertEqual(j.name, {'first' : 'Joe', 'last' : 'Blow'})
+        self.assertEqual(j.age, 1)
+
+    def test_init_kw1(self):
+        j = JsonDict(first='John', last='Doe', age=1)
+        self.assertEqual(j.first, 'John')
+        self.assertEqual(j.last, 'Doe')
+        self.assertEqual(j.age, 1)
+
+    def test_init_kw2(self):
+        j = JsonDict(name__first='John', name__last='Doe', age=1)
+        self.assertEqual(j.name.first, 'John')
+        self.assertEqual(j.name.last, 'Doe')
+        self.assertEqual(j.age, 1)
+
+    def test_bad_key(self):
+        def try_foo(obj):
+            return obj.foo
+
+        j = JsonDict(first='John', last='Doe', age=1)
+        self.assertRaises(AttributeError, try_foo, j)
+
+    def test_class_default1(self):
+        class Widget(JsonDict):
+            _default = {'name': None,
+                        'width': 100,
+                        'height': 200}
+
+        w1 = Widget(name='Box')
+        self.assertEqual(w1.name, 'Box')
+        self.assertEqual(w1.width, 100)
+        self.assertEqual(w1.height, 200)
+
+        w1.width = 150
+        w1.height = 250
+        self.assertEqual(w1.width, 150)
+        self.assertEqual(w1.height, 250)
+
+        w2 = Widget(name='Rect', width=120)
+        self.assertEqual(w2.name, 'Rect')
+        self.assertEqual(w2.width, 120)
+        self.assertEqual(w2.height, 200)
+
+    def test_class_default2(self):
+        class Widget(JsonDict):
+            _default = {'name': None,
+                        'size' : {'width': 100,
+                                  'height': 200}}
+
+        w1 = Widget(name='Box')
+        self.assertEqual(w1.name, 'Box')
+        self.assertEqual(w1.size.width, 100)
+        self.assertEqual(w1.size.height, 200)
+
+        w1.size.width = 150
+        w1.size.height = 250
+        self.assertEqual(w1.size.width, 150)
+        self.assertEqual(w1.size.height, 250)
+
+        w2 = Widget(name='Rect', size__width=120)
+        self.assertEqual(w2.name, 'Rect')
+        self.assertEqual(w2.size.width, 120)
+        self.assertEqual(w2.size.height, 200)
+
+    def test_class_required1(self):
+        class Widget(JsonDict):
+            _default = {'name': None,
+                        'size' : {'width': 100,
+                                  'height': 200}}
+            _required = ['name']
+
+        def try_create():
+            w = Widget()
+
+        self.assertRaises(KeyError, try_create)
+        w = Widget(name='Box')
+        self.assertEqual(w.name, 'Box')
+        
+    def test_class_required1(self):
+        class Widget(JsonDict):
+            _default = {'name': None,
+                        'size' : {'width': None,
+                                  'height': 200}}
+            _required = ['name',
+                         'size__width']
+
+        def try_create():
+            w = Widget(name='Foo')
+
+        self.assertRaises(KeyError, try_create)
+        w = Widget(name='Box', size__width=100)
+        self.assertEqual(w.name, 'Box')
+        self.assertEqual(w.size.width, 100)
+        self.assertEqual(w.size.height, 200)
+
+if __name__ == '__main__':
+    unittest.main()
