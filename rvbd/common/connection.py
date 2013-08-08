5387880d9a2bbb8ba6b910ca9688b1e5c62178cb
Mike Garabedian <mike.garabedian@riverbed.com>
2013-08-08 17:55:11 -0400

    Refactor post_raw/upload_file to single upload method

--- rvbd/common/connection.py
+++ rvbd/common/connection.py
@@ -452,29 +452,7 @@ class Connection(object):
 
         return json.loads(data, object_hook=DictObject.create_from_dict)
 
-    def upload_file(self, urlpath, data, params=None, extra_headers=None):
-        '''Send a JSON rquest to the host.  For POST/PUT requests, the
-        `data` parameter will be JSON-encoded before transmission.  For
-        all requests, the response is JSON-decoded using the
-        DictObject class before returning.'''
-
-        if extra_headers is None:
-            extra_headers = {}
-
-        extra_headers['Content-Type'] = 'application/octet-stream'
-
-        resp = self.request(urlpath, "PUT", data,
-                            params=params, extra_headers=extra_headers)
-
-        data = resp.read()
-        if resp.status == 204:
-            return # no data
-        elif resp.status == 201:
-            # created resource
-            return {'Location-Header': resp.getheader('location', '')}
-        return data
-
-    def post_raw(self, urlpath, data, params=None, extra_headers=None):
+    def upload(self, urlpath, data, method="POST", params=None, extra_headers=None):
         """Upload raw data to the given URL path with the given content type.
 
         `data` may be either a string or a python file object.
@@ -485,10 +463,13 @@ class Connection(object):
         `params` is a dictionary of URL parameters to attach to the request.
             The keys and values will be urlencoded.
 
+        `method` defaults to "POST", but can be overridden if the API requires another
+            method such as "PUT" to be used instead.
+
         Returns location information if resource has been created,
         otherwise the response body (if any).
         """
-        resp = self.request(urlpath, "POST", data, params=params,
+        resp = self.request(urlpath, method, data, params=params,
                             extra_headers=extra_headers)
         data = resp.read()
         if resp.status == 204:
