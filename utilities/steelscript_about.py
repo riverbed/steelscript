0e595b1a9b461b864c8a8617a6ff87a96c92764e
Mike Garabedian <mike.garabedian@riverbed.com>
2014-04-28 13:52:34 -0400

    Enhance about script to report all steelscript pkgs

--- utilities/steelscript_about.py
+++ utilities/steelscript_about.py
@@ -2,58 +2,75 @@
 
 # Copyright (c) 2013 Riverbed Technology, Inc.
 #
-# This software is licensed under the terms and conditions of the 
+# This software is licensed under the terms and conditions of the
 # MIT License set forth at:
-#   https://github.com/riverbed/flyscript/blob/master/LICENSE ("License").  
+#   https://github.com/riverbed/flyscript/blob/master/LICENSE ("License").
 # This software is distributed "AS IS" as set forth in the License.
 
 
-
-import pkg_resources
-import platform
 import sys
+import pkg_resources
 
 try:
-    dist = pkg_resources.get_distribution("flyscript")
-    print "Package 'flyscript' version %s installed" % dist.version
+    dist = pkg_resources.get_distribution('steelscript.common')
 except pkg_resources.DistributionNotFound:
-    print "Package not found: 'flyscript'"
+    print "Package not found: 'steelscript.common'"
     print "Check the installation"
-    #sys.exit(1)
-    
+    sys.exit(1)
 
+
+import os.path
+import platform
 import steelscript
-import os.path, pkgutil
 
 pkgpath = os.path.dirname(steelscript.__file__)
+
+e = pkg_resources.AvailableDistributions()
 
 print ""
 print "Path to source:\n  %s" % pkgpath
 print ""
-print "Modules provided:" 
-for (loader, name, ispkg) in pkgutil.walk_packages([pkgpath]):
-    print "  steelscript.%s" % name
+print "Installed SteelScript Packages"
+print "Core packages:"
+core_pkgs = [x for x in e if x.startswith('steel') and 'appfwk' not in x]
+for p in core_pkgs:
+    pkg = pkg_resources.get_distribution(p)
+    print '%40s - %s' % (pkg.project_name, pkg.version)
 
 print ""
-print "Python information:"
-print 'Version      :', platform.python_version()
-print 'Version tuple:', platform.python_version_tuple()
-print 'Compiler     :', platform.python_compiler()
-print 'Build        :', platform.python_build()
-print 'Architecture :', platform.architecture()
+print "Application Framework packages:"
 
-print ""
-print "Platform information:"
-print platform.platform()
-print 'system   :', platform.system()
-print 'node     :', platform.node()
-print 'release  :', platform.release()
-print 'version  :', platform.version()
-print 'machine  :', platform.machine()
-print 'processor:', platform.processor()
-
-print ""
-print "Python path:"
-print sys.path
+appfwk_pkgs = [x for x in e if x.startswith('steel') and 'appfwk' in x]
+if appfwk_pkgs:
+    for p in appfwk_pkgs:
+        pkg = pkg_resources.get_distribution(p)
+        print '%40s - %s' % (pkg.project_name, pkg.version)
+else:
+    print "None."
 
 
+if '-v' in sys.argv or '--verbose' in sys.argv:
+    print ""
+    print "Python information:"
+    print 'Version      :', platform.python_version()
+    print 'Version tuple:', platform.python_version_tuple()
+    print 'Compiler     :', platform.python_compiler()
+    print 'Build        :', platform.python_build()
+    print 'Architecture :', platform.architecture()
+
+    print ""
+    print "Platform information:"
+    print platform.platform()
+    print 'system   :', platform.system()
+    print 'node     :', platform.node()
+    print 'release  :', platform.release()
+    print 'version  :', platform.version()
+    print 'machine  :', platform.machine()
+    print 'processor:', platform.processor()
+
+    print ""
+    print "Python path:"
+    print sys.path
+else:
+    print ""
+    print "(add -v or --verbose for further information)"
