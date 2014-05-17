858d4b35d274c010a4018459858aa19282e25093
Mike Garabedian <mike.garabedian@riverbed.com>
2014-05-17 11:30:31 -0400

    Update setup.py header

--- setup.py
+++ setup.py
@@ -1,3 +1,9 @@
+# Copyright (c) 2014 Riverbed Technology, Inc.
+#
+# This software is licensed under the terms and conditions of the MIT License
+# accompanying the software ("License").  This software is distributed "AS IS"
+# as set forth in the License.
+
 import os
 from glob import glob
 
