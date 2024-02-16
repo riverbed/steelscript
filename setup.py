696676d2bb785784b2166de47f8cd6830ee48089
Gwen Blum <19484478+gwenblum@users.noreply.github.com>
2024-02-16 22:32:38 +0100

    Update copyright

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2021 Riverbed Technology, Inc.
+# Copyright (c) 2021-2024 Riverbed Technology, Inc.
 #
 # This software is licensed under the terms and conditions of the MIT License
 # accompanying the software ("License").  This software is distributed "AS IS"
